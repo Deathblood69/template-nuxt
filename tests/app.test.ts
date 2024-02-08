cbe1a3de5e5a9740bb2d30e5a52a3908531e8519
MaximusCrowe <deathblood69002@gmail.com>
2024-02-08 14:20:01 +0100

    test(unitaires): Mise en place du shallowMount

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,11 +1,11 @@
 import {describe, expect, test} from 'vitest'
-import {mount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import index from '../../src/app.vue'
 import {vuetify} from '~/plugins/vuetify'
 
 describe("Tests de la page d'erreur", () => {
   test('Affichage de la page', () => {
-    const wrapper = mount(index, {
+    const wrapper = shallowMount(index, {
       global: {
         plugins: [vuetify],
       },
--- tests/components/AppBar.test.ts
+++ tests/components/AppBar.test.ts
@@ -1,11 +1,11 @@
 import {describe, expect, test} from 'vitest'
-import {mount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import index from '../../src/components/AppBar.vue'
 import {vuetify} from '~/plugins/vuetify'
 
 describe('Tests de la page index global', () => {
   test('Affichage de la page', () => {
-    const wrapper = mount(index, {
+    const wrapper = shallowMount(index, {
       props: {
         items: [],
       },
